a8846495e23501cb2f5021d00abe80f9cb7fd00b
Raissa-Santana <raissabarboza136@gmail.com>
2024-01-17 20:42:17 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function sim() {
+    alert("Você aceitou sair comigo, bora marcar logo po!!")
+    location.href = "https://www.youtube.com/watch?v=785bHsX_O-A";
+}
+
+function desviar() {
+    console.log("opa, desviei")
+}
+
+function desvia(btn) {
+    // btn declarado na função
+    btn.style.position = 'absolute';
+    btn.style.bottom = geraPosicao(10, 90);
+    btn.style.left = geraPosicao(10, 90);
+    console.log('opa, desviei...');
+}
+
+function geraPosicao(min, max) {
+    return (Math.random() * (max - min) + min) + "%";
+}
